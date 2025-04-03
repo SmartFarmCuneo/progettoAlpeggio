2077961cc53aeab50b89b0ab8508fc99c2408b72
MarcoGiorgis <marco.giorgis.898@itiscuneo.eu>
2025-04-03 21:43:56 +0200

    updated site

---
+++ site_official/static/php/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+  <script src="../static/assets/js/color-modes.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.122.0">
+  <title>Aggiunta campo</title>
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/checkout/">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+  <link href="../static/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../static/css/aggiungi_campo.css">
+</head>
+
+<div class="container">
+<div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4"
+          src="../static/image/Farm-Logo-Graphics-49002256-1-1-580x363-removebg-preview.png" alt="" width="72"
+          height="57">
+</div>
+</div>
